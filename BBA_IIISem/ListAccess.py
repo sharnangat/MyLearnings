8575e3b3b90368e6c92a2cd2d0d5f3db962b5491
sanjay sharnangat <sanjaysharnangat@gmail.com>
2019-07-20 21:41:14 +0530

    ListAccess.py

---
+++ BBA_IIISem/ListAccess.py
@@ -0,0 +1,12 @@
+list_1=[]
+list_2=list()
+
+
+print("list {}".format(list_1))
+print("list {}".format(list_2))
+
+
+if list_1 == list_2:
+    print("Both list are equal")
+else:
+    print("Both list are not equal")
